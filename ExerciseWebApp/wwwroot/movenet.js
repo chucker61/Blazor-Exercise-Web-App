8ee4d54e64af79c02608c5993b0c66eb6b474ccd
chucker61 <meliksahmertcakir@hotmail.com>
2024-02-13 12:29:11 +0300

    movenet güncellendi

---
+++ ExerciseWebApp/wwwroot/movenet.js
@@ -0,0 +1,38 @@
+﻿let detector;
+
+// Ana işlem
+async function run() {
+    
+    const canvas = document.getElementById('overlay');
+    // Kullanıcıdan kamera izni iste ve başlat
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: false
+            video: {
+                height: 240,
+                width: 320,
+                facingMode : 'user',
+            }
+        });
+        const video = document.getElementById('video');
+        video.srcObject = stream;
+        video = await new Promise((resolve, reject) => {
+            video.onloadedmetadata = () => resolve(videoElement);
+        });
+        video.play();
+
+    } catch (err) {
+        console.error("Kamera erişiminde hata:", err);
+    }
+    if (detector == null) {
+        detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet);
+    }
+
+    const poses = await detector.estimatePoses(video);
+    const keypoints = poses[0].keypoints;
+    return keypoints;
+}
+
+
+
+
