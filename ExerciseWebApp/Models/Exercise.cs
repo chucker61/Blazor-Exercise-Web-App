87fe9c35242cab0be61c4d22e0f51ad529025f42
chucker61 <meliksahmertcakir@hotmail.com>
2024-03-10 12:57:57 +0300

    updated

--- ExerciseWebApp/Models/Exercise.cs
+++ ExerciseWebApp/Models/Exercise.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public string VideoLink { get; set; }
         public bool IsChecked { get; set; } = false;
         public bool IsDone { get; set; } = false;
     }
