bfd7654d04fa2edeeb3cf5857a41d2d1ca6cfcfa
chucker61 <meliksahmertcakir@hotmail.com>
2024-06-03 00:23:44 +0300

    updated

--- ExerciseWebApp/Program.cs
+++ ExerciseWebApp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
